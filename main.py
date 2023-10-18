b84bf9052f34fdd6a2fe2b12deb8762346c80712
93N3515 <70991773+93N3515@users.noreply.github.com>
2023-10-18 13:09:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ zapros = input("запрос (слово-часть ссылки) (ex: vk.)")
 
 try:
     with open(input("входной файл: "),"r+",encoding="utf-8") as inputFile:
-        with open(input("выходной файл: "), "w+",encoding="utf-8") as OutFile:
+        with open(zapros+".txt", "w+",encoding="utf-8") as OutFile:
             for line in inputFile:
                 line = line.replace("http://","").replace("https://","")
                 if line.count(zapros) != 0:
@@ -11,6 +11,6 @@ try:
                         print("ошибка формата - "+ line)
                         continue
                     OutFile.write(split[1]+":"+split[2])
-
+    print("Закончил! Выходной файл - " +zapros+".txt")
 except Exception as ex:
     print(Exception)
