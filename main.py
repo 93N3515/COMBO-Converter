3038c98b0aebd2f7a93450d98bfdfbedb69c6690
93N3515 <70991773+93N3515@users.noreply.github.com>
2023-10-18 13:36:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ zapros = input("запрос (слово-часть ссылки) (ex: vk.)")
 
 
 try:
+    count = 0
     with open(input("входной файл: "),"r+",encoding="utf-8") as inputFile:
         with open("tmp.txt", "w+", encoding="utf-8") as tmpFile:
                 for line in inputFile:
@@ -13,15 +14,17 @@ try:
                         if len(split) != 3:
                             print("ошибка формата - "+ line)
                             continue
+
                         tmpFile.write(split[1]+":"+split[2]+"\r\n")
     with open(zapros + ".txt", "w+", encoding="utf-8") as OutFile:
         lines = list(set(open("tmp.txt", "r+").readlines()))
         for line in lines:
-            if len(line) < 2:
+            if len(line) < 2 and line.count(":") != 2:
                 continue
+            count += 1
             OutFile.write(line)
 
-    print("Закончил! Выходной файл - " +zapros+".txt")
+    print(f"Закончил! Всего {count} Выходной файл - " +zapros+".txt")
     os.remove("tmp.txt")
     input()
 except Exception as ex:
